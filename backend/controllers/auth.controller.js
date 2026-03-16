9bd5134da268bcc07d36b867e59d2da609dc7df3
UsmanRizan <usmanrizan6@gmail.com>
2026-03-16 10:42:27 +0530

    Completed authentication workflow

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,62 @@
+import { redis } from "../lib/redis.js";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.access_Token_secret, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ userId }, process.env.refresh_Token_secret, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refreshToken:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60,
+  ); //Store for 7 days
+};
+
+const setCookie = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true, // prevent cross-site scripting attacks
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // prevent cross-site request forgery attacks
+    maxAge: 15 * 60 * 1000, // 15 minutes
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true, // prevent cross-site scripting attacks
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // prevent cross-site request forgery attacks
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+};
+
 export const signup = async (req, res) => {
-  res.send("Signup route");
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+    const user = await User.create({ name, email, password });
+
+    // authenticate user and generate token here
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookie(res, accessToken, refreshToken);
+
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const login = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-
+// Import routes
 import authRoutes from "./routes/auth.route.js";
-
+import { connectDB } from "./lib/db.js";
+// Load environment variables from .env file
 dotenv.config();
-
+// Create an instance of Express
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/signup", authRoutes);
+app.use(express.json()); // allow parsing of JSON bodies
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+  connectDB();
 });

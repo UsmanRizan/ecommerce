d7b91ad519fda9f9a1abd188d81b58b21f098572
UsmanRizan <usmanrizan6@gmail.com>
2026-04-02 07:27:19 +0530

    couponRoutes Completed

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import productRoutes from "./routes/product.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 import { connectDB } from "./lib/db.js";
 // Load environment variables from .env file
 dotenv.config();
@@ -19,6 +20,7 @@ app.use(cookieParser()); // allow parsing of cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
